292811911ea22525e6e0866e879418a0099e66b5
Allen G <allengryg@gmail.com>
2025-01-02 20:20:39 -0500

    Tire system, not working/integrating

--- main.lua
+++ main.lua
@@ -189,11 +189,12 @@ function mapMouseToSteerAngle()--angles are in radians
     if abs(player.steerAngle) > maxSteerAngle then
         player.steerAngle = maxSteerAngle * player.steerAngle / abs(player.steerAngle)
     end
+    player.front.steerAngle = player.steerAngle
 end
 
 function mapKeystoAcceleration()
     if keys.up then
-        player.accel = 2
+        player.accel = 4
     elseif keys.down then 
         player.accel = -2
     else
@@ -214,9 +215,9 @@ function updateNetForce(target)
     return target.forceApplied + target.forceCentripetal + target.forceFriction
 end
 
-function updateForceApplied(target)
+function updateForceApplied(target)--rear wheel drive
     local magnitude = target.accel * target.mass
-    local direction = target.direction
+    local direction = target.direction --+ target.steerAngle
     return Vector.new(magnitude,direction)
 end
 
@@ -228,35 +229,105 @@ function updateForceFriction(target)
     end
 end
 
-function updateForceCentripetal(target)
+function updateForceCentripetal(target,dt)
     if target.steerAngle == 0 then
+        target.yawRate = 0
         return Vector.new(0,target.steerAngle)
     end
     local radius = target.wheelbase / math.sin(target.steerAngle)
     local forceMagnitude = target.mass * target.v.m * target.v.m / abs(radius)
     local forceAngle = target.direction + target.steerAngle + math.pi/2 * target.steerAngle / abs(target.steerAngle)
     local newForce = Vector.new(forceMagnitude,forceAngle)
+
+    --player.angularVelocity = player.v.m / radius
+    --player.newDirection = player.newDirection + player.angularVelocity * dt
+
+    local d = math.sqrt((target.wheelbase ^ 2)/math.tan(target.steerAngle) ^ 2 + target.rear.longitudinalOffset ^ 2)
+    target.yawRate = target.v.m / d
     return newForce 
 end
 
-
 function carDirection(target)
-    
-    --[[if magnitudeOfVelocity == 0 then
-        return currentDirection
-    end
-
-    local newDirection = 0  
-    newDirection = math.atan2(velocityVector.y,velocityVector.x)
-
-    if abs(currentDirection - newDirection) > math.pi * 170 / math.pi then
-        return newDirection - math.pi
-    else
-        return newDirection
-    end]]
-    
+    direction = math.atan2((target.front.y - target.rear.y),(target.front.x - target.rear.x))
+    target.rear.direction = direction
+    target.front.direction = direction
     return target.v.d
-    
+end
+
+function updateTires(target,dt)
+    target.rear:update(target.direction,target.yawRate,target.forceApplied/2,target.mass/2,dt)
+    target.front:update(target.direction,target.yawRate,target.forceApplied/2,target.mass/2,dt)
+end
+
+local tire = {}
+tire.__index = tire
+tire.longitudinalOffset = 1.5
+tire.lateralOffset = 1
+tireWidth = 10
+tireHeight = 20
+tire.load = 0
+--tire.x = 0
+--tire.y = 0
+tire.v = Vector.new()
+tire.netForce = Vector.new()
+tire.normalForce = 0
+tire.lonForce = Vector.new()
+tire.latForce = Vector.new()
+tire.slipAngle = 0
+tire.steerAngle = 0
+tire.direction = 0
+tire.D = 10 -- magic formula constants
+tire.C = 2
+tire.B = .3
+tire.E = 1
+
+
+function tire.new(typeTire,x,y)
+    local t = {typeTire = typeTire,x = x,y = y}
+    if t.typeTire ~= "rear" and t.typeTire ~= "front" then
+        error("invalid type of tire")
+    end
+    setmetatable(t, tire)
+    if t.side == "rear" then
+        t.longitudinalOffset = - t.longitudinalOffset
+    end
+    return t
+end 
+
+function tire:update(direction,yawRate,force,mass,dt)
+    self.direction = direction
+    self.yawRate = yawRate
+    self.lonForce = force
+    self.load = mass
+    self:updateSlipAngle()
+    self:updateLateralForce()
+    self.netForce = self.lonForce + self.latForce
+    self.netForce.d = self.netForce.d + self.direction
+    self.v = self.v + self.netForce/1000 * dt
+    self.x = self.x + self.v:x() * dt --* scale
+    self.y = self.y + self.v:y() * dt --* scale 
+end
+
+function tire:updateSlipAngle()--
+    local term1 = self.v:y() - self.longitudinalOffset * self.yawRate
+    local term2 = self.v:x() - self.lateralOffset * self.yawRate
+end
+
+function tire:updateLateralForce()-- F(s) = D sin(C * atan(B(1-E)s + E * atan(Bs)))
+    local magnitude = self.D * math.sin(self.C * math.atan(self.B * (1 - self.E) * self.slipAngle + self.E * math.atan(self.B * self.slipAngle)))
+    local direction = self.direction + self.steerAngle + math.pi/2 * self.steerAngle / abs(self.steerAngle)
+    local forceLateral = Vector.new(magnitude,direction)
+    self.latForce = forceLateral
+end
+
+function tire:draw()
+    love.graphics.setColor(love.math.colorFromBytes(129, 252, 170))
+    love.graphics.push()
+    love.graphics.translate(self.x,self.y)
+    love.graphics.rotate(self.direction)
+    love.graphics.rectangle("fill",-tireHeight/2,-player.width/2 - tireWidth/2,tireHeight,tireWidth)
+    love.graphics.rectangle("fill",-tireHeight/2,player.width/2 - tireWidth/2,tireHeight, tireWidth)
+    love.graphics.pop()
 end
 
 function updatePlayer(dt)
@@ -266,8 +337,9 @@ function updatePlayer(dt)
     mapKeystoAcceleration()
     player.forceApplied = updateForceApplied(player)
     player.forceFriction = updateForceFriction(player)
-    player.forceCentripetal = updateForceCentripetal(player)
+    player.forceCentripetal = updateForceCentripetal(player,dt)
     player.forceNet = updateNetForce(player)
+    updateTires(player,dt)
     --if magnitudeOfVelocity == 0 then
         --player.direction = player.direction
     --else
@@ -278,6 +350,8 @@ function updatePlayer(dt)
     player.x = (player.x + player.v:x() * dt * scale)
     player.y = (player.y + player.v:y() * dt * scale) 
 end
+
+
 function love.load()
     w = 1000
     h = 750
@@ -288,12 +362,15 @@ function love.load()
     player = {}
     player.x = w/2--initial position
     player.y = h/2--initial position
-    player.mass = 1000
+    player.mass = 2000
     player.width = 40
     player.height = 60
     player.wheelbase = 3
+    player.yawRate = 0
+    player.angularVelocity = 0
     player.steerAngle = 0
     player.direction = 0
+    player.newDirection = 0
     player.accel = 0
     player.v = Vector.new(0,0)
     player.forceNet = Vector.new(0,0)
@@ -301,6 +378,9 @@ function love.load()
     player.forceFriction = Vector.new(0,0)
     player.forceCentripetal = Vector.new(0,0)
     player.forceNormal = player.mass * 9.81 * .05
+    player.rear = tire.new("rear",player.x,player.y)
+    player.front = tire.new("front",player.x + player.wheelbase,player.y)
+    
     keys = {}
 
     pedal = {}
@@ -313,8 +393,7 @@ function love.load()
     maxSteerAngle = math.pi * 0.33
     pedal.maxWidth = (pedal.w - 10) / 2
 
-    tireWidth = 10
-    tireHeight = 20
+    
 end
 
 function love.update(dt) 
@@ -353,6 +432,8 @@ function love.draw()
             love.graphics.rectangle("fill",-tireHeight/2,- tireWidth/2,tireHeight,tireWidth)
         love.graphics.pop()
     love.graphics.pop()
+    player.rear:draw()
+    player.front:draw()
     printVectors(player.x,player.y)
     drawPedal()
     
