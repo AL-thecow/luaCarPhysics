81b16eff55482b586b5234eb339362dc5e86aae3
Allen G <allengryg@gmail.com>
2025-02-10 11:06:19 -0500

    split up vector normalization test

--- test.lua
+++ test.lua
@@ -23,15 +23,17 @@ TestVector = {}
         luaunit.assertEquals(unit.m,1)
         luaunit.assertEquals(unit.d,math.pi/3)
     end
-    function TestVector:testVectorNormalize()
+    function TestVector:testVectorNormalizePositiveRadians()
         local v = Vector.new(5,7 * math.pi/3)
         local testNormal = normalize(v)
-        --luaunit.assertEquals(testNormal.m,v.m)
-        --luaunit.assertEquals(testNormal.d,math.pi/3)
-        local v2 = Vector.new(5,-5 * math.pi/3)
-        local testNormal2 = normalize(v2)
-        luaunit.assertEquals(testNormal2.m,v2.m)
-        luaunit.assertEquals(testNormal2.d,math.pi/3)
+        luaunit.assertEquals(testNormal.m,v.m)
+        luaunit.assertEquals(testNormal.d,math.pi/3)
+    end
+    function TestVector:testVectorNormalizeNegativeRadians()
+        local v = Vector.new(5,-5 * math.pi/3)
+        local testNormal = normalize(v)
+        luaunit.assertEquals(testNormal.m,v.m)
+        luaunit.assertEquals(testNormal.d,math.pi/3)
     end
     function TestVector:testVectorComponents()
         luaunit.assertEquals(test1:x(),test1.m * math.cos(test1.d))
@@ -44,16 +46,5 @@ TestVector = {}
         luaunit.assertEquals(dot1,dot2)
         luaunit.assertEquals(dot1,testdot)
     end
-
--- TestVector:vectorAddition()
-
--- function testVectorAddition()
---     test1 = Vector.new(5,math.pi/3)
---     test2 = Vector.new(5,math.pi/3)
---     local sum = test1 + test2
---     luaunit.assertEquals(sum.m,10)
---     luaunit.assertEquals(sum.d,math.pi/3)
--- end
-
 
 os.exit( luaunit.LuaUnit.run() )
